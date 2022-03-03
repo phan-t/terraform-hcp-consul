4cb9e4da8be19ea2b66433ad944f6b61df17e939
Tony Phan <tphan@hashicorp.com>
2022-03-03 20:55:33 +1100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# terraform-hcp-consul
+# HCP Consul Demo with EKS
+
+**Build time is ~15 minutes**
+
+## How to use this module
+
+### Post Deployment
+#### Create `kubeconfig` for Amazon EKS
+```shell
+aws eks --region $(terraform output -raw aws_region) update-kubeconfig --name $(terraform output -raw deployment_id)
+```
+#### Retrieve Consul root token
+```shell
+terraform output consul_root_token
+```
---
+++ variables.tf
@@ -0,0 +1,47 @@
+variable "deployment_name" {
+  description = "Deployment name, used to prefix resources"
+  type        = string
+  default     = ""
+}
+
+variable "owner" {
+  description = "Resource owner identified using an email address"
+  type        = string
+  default     = ""
+}
+
+variable "ttl" {
+  description = "Resource TTL (time-to-live)"
+  type        = number
+  default     = 48
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = ""
+}
+
+variable "aws_key_pair_key_name" {
+  description = "Key pair name"
+  type        = string
+  default     = ""
+}
+
+variable "hcp_region" {
+  description = "HCP region"
+  type        = string
+  default     = ""
+}
+
+variable "hcp_client_id" {
+  description = "HCP client id"
+  type        = string
+  default     = ""
+}
+
+variable "hcp_client_secret" {
+  description = "HCP client secret"
+  type        = string
+  default     = ""
+}
